86612b9529dce6541d09096a40545e9dd00d0775
ElijahUkeme <ukemedmet@gmail.com>
2023-05-07 13:42:01 -0700

    Response class for customer registration

---
+++ src/main/java/com/elijah/foodorderingproject/response/RegistrationResponse.java
@@ -0,0 +1,15 @@
+package com.elijah.foodorderingproject.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationResponse {
+
+    private String message;
+    private String token;
+}
